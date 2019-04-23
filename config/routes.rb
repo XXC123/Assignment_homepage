a3905f037f9c2c10221083a4d5712706c8f668d2
XXC123 <s3561226@student.rmit.edu.au>
2019-04-23 10:25:14 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  #root 'static_pages#Category' 
+  get 'static_pages/Category'
+
+  get 'static_pages/Locations'
+
+  get 'static_pages/Login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should get root"do
+    get root_url
+    assert_response :success
+    end
+
+  
+  test "should get Category"do
+    get static_pages_Category_url
+    assert_response :success
+    assert_select "title", "Category | Category page" 
+  end
+  
+  test "should get Locations"do
+    get static_pages_Locations_url
+    assert_response :success
+    assert_select "title", "Locations | Location page" 
+  end
+  
+  test "should get Login"do
+    get static_pages_Login_url
+    assert_response :success
+    assert_select "title", "Login | Login page" 
+  end
+
+end
